d46de9e05d740a1ec82525db07a7a8426d699a9b
SyedMohiuddin <msyed199343@gmail.com>
2023-02-01 04:00:20 -0600

    dragon class

--- classes/dragon.js
+++ classes/dragon.js
@@ -1,5 +1,22 @@
 // Your code here
+class Dragon {
+  constructor(name, color){
+    this.name = name
+    this.color = color
+  }
 
+  breathesFire(){
+    return `${this.name} breathes fire everywhere! BURN!!!!`
+  }
+
+  static getDragons(...dragonInstances){
+    return dragonInstances.map(dragon => dragon.name)
+  }
+}
+
+const puff = new Dragon("Puff", "green");
+console.log(puff);
+console.log(puff.breathesFire());
 /****************************************************************************/
 /******************* DO NOT EDIT CODE BELOW THIS LINE ***********************/
 
