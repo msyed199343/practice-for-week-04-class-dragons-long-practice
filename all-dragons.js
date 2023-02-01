d6f6a5fa69e0d3e23a0a28507ad5a5bfcc3362e6
SyedMohiuddin <msyed199343@gmail.com>
2023-02-01 04:58:06 -0600

    completed

--- all-dragons.js
+++ all-dragons.js
@@ -1,5 +1,31 @@
 // Your code here
+const FriendlyDragon = require('./classes/friendly-dragon')
+const EvilDragon = require('./classes/evil-dragon')
+const Dragon = require('./classes/dragon')
 
+let lifeGoalsOne = ["save Atreyu from the swamp", "save Atreyu from the Nothing",
+ "scare the local bullies into a dumpster"]
+
+let falkor = new FriendlyDragon('Falkor', 'white', lifeGoalsOne, "Bastian")
+
+let evilDoingsOne = [ "take over your mountain kingdom", "steal all your dwarven gold",
+"burn down your floating village"]
+
+let smaug = new EvilDragon("Smaug", "black", evilDoingsOne, "Dwarf King")
+
+let allDragons = Dragon.getDragons(falkor, smaug)
+
+// console.log(falkor);
+// falkor.hasLifeGoals();
+// console.log(falkor.breathesFire());
+// console.log(falkor.helpsPeople());
+
+// console.log(smaug);
+// smaug.dontInviteThemOverForDinner();
+// console.log(smaug.breathesFire());
+// console.log(smaug.burnsNemesis());
+
+console.log(allDragons)
 
 /****************************************************************************/
 /******************* DO NOT EDIT CODE BELOW THIS LINE ***********************/
@@ -7,6 +33,7 @@
 try {
   module.exports.falkor = falkor
   module.exports.smaug = smaug
+  module.exports.allDragons = allDragons
 } catch {
   module.exports = null;
 }
